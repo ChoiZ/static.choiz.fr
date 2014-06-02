8fd431d4b0d1c230d1e760c2ee4c2b146a72b72c
François LASSERRE <choiz@me.com>
2014-06-02 19:48:13 +0200

    Update files

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,14 @@ module.exports = function (grunt) {
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
 
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
     grunt.initConfig({
 
         pkg: grunt.file.readJSON('package.json'),
@@ -17,36 +25,80 @@ module.exports = function (grunt) {
             prod: 'static/'
         },
 
+        csslint: {
+            dev: {
+                options: {
+                    /* https://github.com/CSSLint/csslint/wiki/Rules */
+                },
+                src: ['<%= config.dev %>*.css']
+            }
+        },
+
+        cssmin: {
+            dev: {
+                files: {
+                    '<%= config.prod %>min.css': ['<%= config.dev %>*.css']
+                }
+            }
+        },
+
         uglify: {
             options: {
-                banner: '/* <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %>* /\n',
-                beautify: {
-                    width:80,
-                    beautify: true
-                }
+                'banner': '/* <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %>* /\n',
+                'preserveComments': false
             },
-            dest: {
+            dev: {
                 files: [{
                     src: '<%= config.dev %>*.js',
-                    dest: '<%= config.prod %>core.js'
+                    dest: '<%= config.prod %>min.js'
                 }]
             }
         },
+
         jshint: {
             options: {
                 jshintrc: '.jshintrc',
                 reporter: require('jshint-stylish')
             },
+            gruntfile: ['Gruntfile.js'],
+            dev: ['<%= config.dev %>*.js'],
             all: ['Gruntfile.js', '<%= config.dev %>*.js']
+        },
+
+        watch: {
+            gruntfile: {
+                files: 'Gruntfile.js',
+                tasks: ['jshint:gruntfile']
+            },
+            js: {
+                files: ['<%= config.dev %>*.js'],
+                tasks: ['jshint:dev','uglify:dev']
+            },
+            css: {
+                files: ['<%= config.dev %>style.css'],
+                tasks: ['csslint:dev','cssmin:dev']
+            }
         }
+
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('lint', [
+        'csslint:dev',
+        'jshint:dev'
+    ]);
+
+    grunt.registerTask('dev', [
+        'csslint:dev',
+        'cssmin:dev',
+        'jshint:dev',
+        'uglify:dev'
+    ]);
 
     grunt.registerTask('default', [
-        'jshint',
-        'uglify:dest'
+        'csslint:dev',
+        'cssmin:dev',
+        'jshint:all',
+        'uglify:dev'
     ]);
 
 };
